f463f95e0e6a895960e06f4067d0691dc075085c
Kajtek-hub <gkajetan@student.agh.edu.pl>
2026-05-18 03:31:29 +0200

    15. Step 05 - Understanding Loose Coupling and Tight Coupling

--- src/main/java/com/kajtek/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/kajtek/learn_spring_framework/AppGamingBasicJava.java
@@ -2,12 +2,15 @@ package com.kajtek.learn_spring_framework;
 
 import com.kajtek.learn_spring_framework.game.GameRunner;
 import com.kajtek.learn_spring_framework.game.MarioGame;
+import com.kajtek.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
-		var marioGame = new MarioGame();
-		var gameRunner = new GameRunner(marioGame);
+		// var marioGame = new MarioGame();
+		
+		var superContraGame = new SuperContraGame();
+		var gameRunner = new GameRunner(superContraGame);
 		gameRunner.run();
 
 	}
