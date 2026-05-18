0c1e61750663e410ce71ba6c12c30cadf1c36887
Kajtek-hub <gkajetan@student.agh.edu.pl>
2026-05-18 02:35:18 +0200

    Initial commit

---
+++ src/main/java/com/kajtek/learn_spring_framework/AppGamingBasicJava.java
@@ -0,0 +1,15 @@
+package com.kajtek.learn_spring_framework;
+
+import com.kajtek.learn_spring_framework.game.GameRunner;
+import com.kajtek.learn_spring_framework.game.MarioGame;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		var marioGame = new MarioGame();
+		var gameRunner = new GameRunner(marioGame);
+		gameRunner.run();
+
+	}
+
+}
---
+++ src/main/java/com/kajtek/learn_spring_framework/game/MarioGame.java
@@ -0,0 +1,5 @@
+package com.kajtek.learn_spring_framework.game;
+
+public class MarioGame {
+
+}
