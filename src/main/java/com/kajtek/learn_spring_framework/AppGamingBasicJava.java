d3dff6836b9088724afe35ea84fe894385918b85
Kajtek-hub <gkajetan@student.agh.edu.pl>
2026-05-19 02:10:55 +0200

    19. Step 07 - Bringing in Spring Framework to Make Java App Loosely Coupled

--- src/main/java/com/kajtek/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/kajtek/learn_spring_framework/AppGamingBasicJava.java
@@ -10,8 +10,9 @@ public class AppGamingBasicJava {
 	public static void main(String[] args) {
 		//var game = new MarioGame();
 		//var game = new SuperContraGame();
-		var game = new PacmanGame();
-		var gameRunner = new GameRunner(game);
+		var game = new PacmanGame();	//1. Tworzymy obiekt
+		var gameRunner = new GameRunner(game); //Wstrzykujemy obiekt w klasę gameRunner
+		// "game" jest zależnością (dependency) klasy "GameRunner", czyli to jest to tzw. Dependency Injection - wstrzykiwanie zależności 
 		gameRunner.run();
 
 	}
