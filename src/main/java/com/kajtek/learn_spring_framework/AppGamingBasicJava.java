34fc65b29560a7e1244918c3b4feff8623705d98
Kajtek-hub <gkajetan@student.agh.edu.pl>
2026-05-19 02:03:20 +0200

    18. Step 06 - 03 - Exercise - Adding Pacman Game

--- src/main/java/com/kajtek/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/kajtek/learn_spring_framework/AppGamingBasicJava.java
@@ -3,13 +3,14 @@ package com.kajtek.learn_spring_framework;
 import com.kajtek.learn_spring_framework.game.GameRunner;
 import com.kajtek.learn_spring_framework.game.MarioGame;
 import com.kajtek.learn_spring_framework.game.SuperContraGame;
+import com.kajtek.learn_spring_framework.game.PacmanGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
-		var game = new MarioGame();
-		
+		//var game = new MarioGame();
 		//var game = new SuperContraGame();
+		var game = new PacmanGame();
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 
