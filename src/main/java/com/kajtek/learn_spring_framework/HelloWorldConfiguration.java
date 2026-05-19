3cf017193771c5df7285b89a047e0e1440b40824
Kajtek-hub <gkajetan@student.agh.edu.pl>
2026-05-19 03:18:02 +0200

    23. Step 09 - Creating More Java Spring Beans in Spring Java Configuration File

--- src/main/java/com/kajtek/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/kajtek/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,11 +3,31 @@ package com.kajtek.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) {};
+record Adress (String firstLine, String City) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	
 	@Bean
 	public String name(){
-		return "Torres";
+		return "Kajta";
+	}
+	
+	@Bean
+	public int age() {
+		return 25;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Taelia",18);
+		//person.name(); person.age();
+	}
+	
+	@Bean
+	public Adress address() {
+		return new Adress("Salma street","Divinty's Reach");
+		
 	}
 }
