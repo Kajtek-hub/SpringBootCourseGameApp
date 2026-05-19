af140f0aee2e220b019bbc10bd3f6bd3eb11dc28
Kajtek-hub <gkajetan@student.agh.edu.pl>
2026-05-19 03:41:28 +0200

    25. Step 10 - 02 - Reusing Existing Spring Beans for Auto Wiring

--- src/main/java/com/kajtek/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/kajtek/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.kajtek.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age) {};
+record Person (String name, int age, Address address) {};
 record Address (String firstLine, String city) {};
 
 @Configuration
@@ -21,13 +21,27 @@ public class HelloWorldConfiguration {
 	
 	@Bean
 	public Person person() {
-		return new Person("Taelia",18);
+		return new Person("Taelia",18, new Address ("Ossan Quarter","Divinty's Reach"));
 		//person.name(); person.age();
+	}
+	
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(),age(), address());
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) {
+		return new Person(name,age, address3);
 	}
 	
 	@Bean (name = "address2")
 	public Address address() {
 		return new Address("Salma street","Divinty's Reach");
-		
+	}
+	
+	@Bean (name = "address3")
+	public Address address3() {
+		return new Address("Rurikton","Divinty's Reach");
 	}
 }
