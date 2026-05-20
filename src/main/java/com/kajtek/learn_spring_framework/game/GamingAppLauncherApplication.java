250daad35c8c7e32aaa5c49450426f8b14aa58e5
Kajtek-hub <gkajetan@student.agh.edu.pl>
2026-05-20 04:18:02 +0200

    45. Step 04 - 01 - Exploring Spring Framework - Different Types of Dependency

--- src/main/java/com/kajtek/learn_spring_framework/game/GamingAppLauncherApplication.java
+++ src/main/java/com/kajtek/learn_spring_framework/game/GamingAppLauncherApplication.java
@@ -1,15 +1,9 @@
-package com.kajtek.learn_spring_framework;
+package com.kajtek.learn_spring_framework.game;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.kajtek.learn_spring_framework.game.GameRunner;
-import com.kajtek.learn_spring_framework.game.GamingConsole;
-import com.kajtek.learn_spring_framework.game.MarioGame;
-import com.kajtek.learn_spring_framework.game.SuperContraGame;
-import com.kajtek.learn_spring_framework.game.PacmanGame;
 
 
 
